f43c0cb9e05053de8c57700adf19167d0300d610
Kaley <kaleyywood@gmail.com>
2026-01-21 20:25:58 -0700

    complete classes - rectangle with square as a sub class

--- fclasses.js
+++ fclasses.js
@@ -1,5 +1,25 @@
 // ...
 
+class Rectangle{
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+    getArea(){
+        return this.width * this.height;
+    }
+
+    getPerimeter(){
+        return 2* (this.width + this.height);
+    }
+}
+
+class Square extends Rectangle{
+    constructor(side){
+        super(side, side);
+    }
+}
+
 const rect = new Rectangle(10, 20);
 
 console.log('Rectangle area: ' + rect.getArea());
